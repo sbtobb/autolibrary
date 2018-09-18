639d722be374649085c526d0ce2bfb9842430be9
sbtobb <tobbcyg@gmail.com>
2018-09-18 10:34:16 +0800

    完善管理员 和 设备 操作接口设计

--- src/main/java/com/cyouguang/autolibrary/service/AdminService.java
+++ src/main/java/com/cyouguang/autolibrary/service/AdminService.java
@@ -1,6 +1,11 @@
 package com.cyouguang.autolibrary.service;
 
+import com.cyouguang.autolibrary.entity.*;
 import com.cyouguang.autolibrary.pojo.StatusMessagePojo;
+import com.cyouguang.autolibrary.pojo.xyPojo;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author CyouGuang
@@ -14,4 +19,135 @@ public interface AdminService {
      * @return StatusMessagePojo 200 登录成功
      */
     StatusMessagePojo login(String loginName, String password);
+
+    /**
+     * 取到分页用户信息表
+     * @param currentPage 页数
+     * @param pageSize 页大小
+     * @return 用户信息 list
+     */
+    List<UserInfo> getUserInfoPage(int currentPage,int pageSize);
+
+    /**
+     * 修改用户积分
+     * @param userInfoId 用户id
+     * @param integral 积分
+     * @return 状态信息码
+     */
+    StatusMessagePojo changeIntegral(int userInfoId,double integral);
+
+    /**
+     * 分页取出图书信息
+     * @param currentPage 页数
+     * @param pageSize 页大小
+     * @return BookInfo List
+     */
+    List<BookInfo> getBookInfoPage(int currentPage,int pageSize);
+
+    /**
+     * 取出图书id下所有标签
+     * @param bookInfoId 书籍信息id
+     * @return BookTag list
+     */
+    List<BookTag> getBookTagList(int bookInfoId);
+
+    /**
+     * 返回书籍借还书列表
+     * @param currentPage 页数
+     * @param pageSize 页大小
+     * @return BookBorrowLog list
+     */
+    List<BookBorrowLog> getBookBorrowLogPage(int currentPage,int pageSize);
+
+    /**
+     * 取设备信息
+     * @param currentPage 页数
+     * @param pageSize 页大小
+     * @return DeviceMaster list
+     */
+    List<DeviceMaster> getDeviceMasterPage(int currentPage,int pageSize);
+
+    /**
+     * 取得订单信息
+     * @param currentPage 页数
+     * @param pageSize 页大小
+     * @return OrderMaster list
+     */
+    List<OrderMaster> getOrderMaster(int currentPage,int pageSize);
+
+    /**
+     * 取得订单详情列表
+     * @param orderId 订单id
+     * @return OrderProduct list
+     */
+    List<OrderProduct> getOrderProduct(int orderId);
+
+    /**
+     * 从isbn号码取到图书信息
+     * @param isbn isbn号码
+     * @return bookinfo
+     */
+    BookInfo getBookInfoFromISBN(String isbn);
+
+    /**
+     * 添加图书
+     * @param bookInfo 图书信息
+     * @return 状态信息码
+     */
+    StatusMessagePojo addBookWithBookInfo(BookInfo bookInfo);
+
+    /**
+     * 添加电子标签
+     * @param bookInfoId 图书id
+     * @param tag 电子标签
+     * @return 状态信息码
+     */
+    StatusMessagePojo addBookTag(int bookInfoId,String tag);
+
+    /**
+     * 取出销售总额
+     * @param beginDate 开始时间
+     * @param endDate 结束时间
+     * @return 总额
+     */
+    double getSaleTotalWithDate(Date beginDate,Date endDate);
+
+    /**
+     * 取出单日销售信息
+     * @param beginDate 开始时间
+     * @param endDate 结束时间
+     * @return x 日期 y 销售额
+     */
+    List<xyPojo> getSalesWithDate(Date beginDate,Date endDate);
+
+    /**
+     * 取出今日销售额
+     * @return 销售额
+     */
+    double getSaleToday();
+
+    /**
+     * 取出访问人数
+     * @param beginDate 开始时间
+     * @param endDate 结束时间
+     * @return 总访问人数
+     */
+    int getVisterTotalWithDate(Date beginDate,Date endDate);
+
+    /**
+     * 取出单日访问人数
+     * @param beginDate 开始时间
+     * @param endDate 结束时间
+     * @return x 日期 y 访问人数
+     */
+
+    List<xyPojo> getVistersWithDate(Date beginDate,Date endDate);
+
+    /**
+     * 取出今日访问人数
+     * @return 访问人数
+     */
+    int getVisterToday();
+
+
 }
--- src/main/java/com/cyouguang/autolibrary/service/DeviceService.java
+++ src/main/java/com/cyouguang/autolibrary/service/DeviceService.java
@@ -1,8 +1,53 @@
 package com.cyouguang.autolibrary.service;
 
+import com.cyouguang.autolibrary.entity.OrderMaster;
+import com.cyouguang.autolibrary.pojo.StatusMessagePojo;
+
 /**
  * @author CyouGuang
  * @date 2018/9/17
  */
 public interface DeviceService {
+    /**
+     * 创建订单
+     * @param deviceId 设备id
+     * @return OrderMaster
+     */
+    OrderMaster createOrder(int deviceId);
+
+    /**
+     * 添加图书
+     * @param orderId 订单id
+     * @param tag 图书tag标签
+     * @return 状态信息码
+     */
+    StatusMessagePojo addProduct(int orderId,String tag);
+
+    /**
+     * 获取到订单信息
+     * @param orderId 订单号码
+     * @return 订单信息
+     */
+    OrderMaster getOrderMaster(int orderId);
+
+    /**
+     * 归还书籍
+     * @param tag 书籍tag
+     * @return 状态信息码
+     */
+    StatusMessagePojo borrowBook(String tag);
+
+    /**
+     * 设备上线
+     * @param deviceId 设备id
+     * @return 状态信息码
+     */
+    StatusMessagePojo online(int deviceId);
+
+    /**
+     * 设备下线
+     * @param deviceId 设备id
+     * @return 状态信息码
+     */
+    StatusMessagePojo offline(int deviceId);
 }
